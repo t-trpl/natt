c0245bfc5ac7070ed501fbe231a0a23ea84aa547
t-trpl <tylertriplett@protonmail.com>
2025-03-03 01:07:01 -0600

    remove old readme

---
+++ build/CMakeFiles/natt.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/natt.dir/src/daemon.c.o"
+  "CMakeFiles/natt.dir/src/daemon.c.o.d"
+  "CMakeFiles/natt.dir/src/gtk_creation.c.o"
+  "CMakeFiles/natt.dir/src/gtk_creation.c.o.d"
+  "CMakeFiles/natt.dir/src/io.c.o"
+  "CMakeFiles/natt.dir/src/io.c.o.d"
+  "CMakeFiles/natt.dir/src/main.c.o"
+  "CMakeFiles/natt.dir/src/main.c.o.d"
+  "CMakeFiles/natt.dir/src/util.c.o"
+  "CMakeFiles/natt.dir/src/util.c.o.d"
+  "CMakeFiles/natt.dir/src/xsctcf.c.o"
+  "CMakeFiles/natt.dir/src/xsctcf.c.o.d"
+  "natt"
+  "natt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/natt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
